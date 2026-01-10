da2ab58baf3b700d4250c27841ac52d6b1376221
Kaan Alpar <kaanalpar33@gmail.com>
2026-01-10 13:32:22 +0300

    Axis Input

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+var speed := 120
+
 func _physics_process(_delta: float) -> void:
-	velocity = Vector2(100, 50)
+	var horizontal_dir := Input.get_axis("move_left", "move_right")
+	var vertical_dir := Input.get_axis("move_up", "move_down")
+	
+	var direction := Vector2(horizontal_dir, vertical_dir).normalized()
+	
+	velocity = speed * direction
 	move_and_slide()
