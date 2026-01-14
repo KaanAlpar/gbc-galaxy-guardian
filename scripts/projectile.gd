35ece9b05f5a5698db279c791abbc291a4938f72
Kaan Alpar <kaanalpar33@gmail.com>
2026-01-14 17:01:01 +0300

    Delta Time

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-var speed := 5.0
+var speed := 300.0
 
 func _physics_process(delta: float) -> void:
-	# global_position = global_position + speed
-	global_position.x += speed
+	global_position.x += speed * delta
