ad7325e50792e733889ab4aade31044f01909a09
Kaan Alpar <kaanalpar33@gmail.com>
2026-01-15 21:37:11 +0300

    VisibleOnScreenNotifier2D

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -4,3 +4,6 @@ var speed := 300.0
 
 func _physics_process(delta: float) -> void:
 	global_position.x += speed * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
