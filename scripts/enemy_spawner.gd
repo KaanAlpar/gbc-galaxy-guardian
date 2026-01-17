ad7b6cebf657d78d0d2d0a2ff5eac5a7e9318ee1
Kaan Alpar <kaanalpar33@gmail.com>
2026-01-17 21:54:10 +0300

    Adding Difficulty

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -3,8 +3,12 @@ extends Node2D
 signal enemy_spawned(instance)
 
 @onready var spawn_positions: Node2D = $SpawnPositions
+@onready var spawn_timer: Timer = $SpawnTimer
 
 var enemy_scene: PackedScene = preload("res://scenes/enemy.tscn")
+
+var difficulty_increase := 0.25
+var minimum_difficulty := 0.25
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_enemy()
@@ -16,3 +20,7 @@ func spawn_enemy() -> void:
 	var enemy_instance = enemy_scene.instantiate()
 	enemy_instance.global_position = random_spawn_position.global_position
 	enemy_spawned.emit(enemy_instance)
+
+func _on_difficulty_timer_timeout() -> void:
+	if spawn_timer.wait_time > minimum_difficulty:
+		spawn_timer.wait_time -= difficulty_increase
