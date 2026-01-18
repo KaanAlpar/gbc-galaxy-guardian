e20b0cbd28502a825e6fd36f07e5fb94fdfbea1f
Kaan Alpar <kaanalpar33@gmail.com>
2026-01-18 19:39:39 +0300

    Projectile Hit State

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 var speed := 300.0
 
 func _physics_process(delta: float) -> void:
@@ -11,3 +14,10 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area is Enemy:
 		area.die()
+		speed = 0.0
+		collision_shape_2d.set_deferred("disabled", true)
+		animated_sprite_2d.play("hit")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "hit":
+		queue_free()
